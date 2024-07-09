e413b8206d08f9f181bf1caac282f9eb4c7cec74
mathispappo <mathispappo@gmail.com>
2024-07-09 17:52:26 +0200

    implementation de back

--- src/components/ChatPage.js
+++ src/components/ChatPage.js
@@ -2,61 +2,30 @@ import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import bar_chat from '../assets/bar_chat.png';
 import logo from '../assets/image.png';
+import Conversation from './Conversation.js';
+
 
 function ChatPage() {
   const [conversations, setConversations] = useState([]);
-  const [currentConversation, setCurrentConversation] = useState(null);
-  const [messages, setMessages] = useState([]);
-  const [message, setMessage] = useState('');
+  const [currentConversationId, setCurrentConversationId] = useState(null);
 
   useEffect(() => {
     loadConversationLinks();
 }, []);
 
 const loadConversationLinks = () => {
-    fetch('https://travel-mate-2.azurewebsites.net/conversation_links')
-        .then(response => response.json())
-        .then(data => setConversations(data));
-};
-
-const loadConversation = (conversationId) => {
-    fetch(`https://travel-mate-2.azurewebsites.net/conversation/${conversationId}`)
-        .then(response => response.json())
-        .then(data => {
-            setMessages(data);
-            setCurrentConversation(conversationId);
-        });
+  fetch('https://travel-mate-2.azurewebsites.net/conversation_links')
+      .then(response => response.json())
+      .then(data => setConversations(data));
 };
 
 const startNewConversation = () => {
-    fetch('https://travel-mate-2.azurewebsites.net/new_conversation', { method: 'POST' })
-        .then(response => response.json())
-        .then(data => {
-            setCurrentConversation(data.conversation_id);
-            setMessages([]);
-            loadConversationLinks();
-        });
-};
-
-const sendMessage = (e) => {
-    e.preventDefault();
-    if (message.trim() === '') return;
-
-    const userMessage = { user_message: message, bot_response: '' };
-    setMessages([...messages, userMessage]);
-
-    fetch('https://travel-mate-2.azurewebsites.net/chat', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ message: message, conversation_id: currentConversation })
-    })
-        .then(response => response.json())
-        .then(data => {
-            const updatedMessages = [...messages, { user_message: message, bot_response: data.bot_response }];
-            setMessages(updatedMessages);
-            setMessage('');
-            setCurrentConversation(data.conversation_id);
-        });
+  fetch('https://travel-mate-2.azurewebsites.net/new_conversation', { method: 'POST' })
+      .then(response => response.json())
+      .then(data => {
+          setCurrentConversationId(data.conversation_id);
+          loadConversationLinks();
+      });
 };
 
   return (
@@ -66,46 +35,47 @@ const sendMessage = (e) => {
       </Link>
       <div style={historiqueStyle}>
         <div style={buttonContainerStyle}>
-            <div style={buttonStyle} onClick={startNewConversation}>
-                <img src={logo} style={chatLogo} alt="logo" />
-                <p>Nouveau chat</p>
-            </div>
-            <div style={underlineStyle}></div>
+          <button style={buttonStyle} onClick={startNewConversation}>
+            <img src={logo} style={chatLogo} alt="logo" />
+            Nouveau chat
+          </button>
+          <div style={underlineStyle}></div>
         </div>
         <div style={discutionStyle}>
-          {conversations.map((conversation_id, index) => (
-            <Link to={`/chat/${conversation_id}`} key={index} style={conversationStyle}>
-              Conversation {conversation_id}
-            </Link>
+          {conversations.map(conversationId => (
+            <a 
+              key={conversationId} 
+              href="#" 
+              style={destinationStyle} 
+              onClick={() => setCurrentConversationId(conversationId)}
+              >
+                Conversation {conversationId}
+            </a>
           ))}
-        </div>
-        <div style={discutionStyle}>
-          <div style={destinationStyle}>Monaco</div>
-          <div style={destinationStyle}>Venezuela</div>
-          <div style={destinationStyle}>New York</div>
-          <div style={destinationStyle}>Essaouira</div>
-          <div style={destinationStyle}>Tokyo</div>
         </div>
       </div>
       <div style={mainStyle}>
-        <div style={helloStyle}>
+        <div style={cadrantstyle}>
+          <div style={helloStyle}>
             <img src={logo} style={logoStyle} alt="logo" />
             <p>
               Bonjour comment puis-je vous aider aujourd’hui ?
             </p>
           </div>
-        <div style={messageStyle}>
-          Je me trouve a Osaka donne moi une bonne adresse de restaurant pas trop chere pour manger avec mes enfants.
+          {currentConversationId && <Conversation conversationId={currentConversationId} />}
         </div>
-        <div style={MessageIAStyle}>
-          Okonomiyaki Chitose est un choix populaire parmi les locaux et les visiteurs. Ce restaurant convivial est réputé pour ses okonomiyaki délicieux et à prix raisonnable. Il est situé dans le quartier animé de Namba, ce qui le rend facile d'accès après une journée de visites.
-        </div>
-        <div style={messsageBoxStyle}>
-          <p>Message à TravelIA...</p>
-        </div>
+      
+        
       </div>
     </div>
   );
+}
+
+const cadrantstyle = {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
 }
 
 const AppStyle = {
@@ -130,7 +100,6 @@ const ButtonAccueilStyle = {
   color: 'white',
   textDecoration: 'none'
 };
-  
 
 const historiqueStyle = {
   backgroundImage: `url(${bar_chat})`,
@@ -146,14 +115,19 @@ const mainStyle = {
   flexDirection: 'column',
   width: '80%',
   alignItems: 'center'
-  //justify-content: center;
-  //flex-grow: 1;
 };
 
 const discutionStyle = {
   color: 'black',
   left: '20%',
   margin: '20px 0 0 0',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  maxHeight: '82vh',
+  overflow: 'auto', // Keep auto to allow scrolling
+  scrollbarWidth: 'none', // For Firefox
+  msOverflowStyle: 'none' // For IE and Edge
 };
 
 const destinationStyle = {
@@ -163,15 +137,10 @@ const destinationStyle = {
   background: 'linear-gradient(to right, #F3FBFF, #D2EEFF)',
   borderRadius: '50px',
   fontWeight: '500',
-};
-
-const conversationStyle = {
-  margin: '10px 0',
-  padding: '10px',
-  border: '1px solid #ccc',
-  borderRadius: '5px',
+  cursor: 'pointer',
+  color: 'black',
   width: '200px',
-  textAlign: 'center',
+  textDecoration: 'none',
 };
 
 const buttonContainerStyle = {
@@ -183,18 +152,15 @@ const buttonContainerStyle = {
 const buttonStyle = {
   display: 'flex',
   background: 'rgba(240, 240, 240, 0.7)',
-  border: '1px solid white', 
+  border: '1px solid white',
   flexDirection: 'row',
   fontWeight: 'bold',
   fontSize: 'larger',
   alignItems: 'center',
   justifyContent: 'center',
   borderRadius: "50px",
-  marginTop: "10px",
-  marginLeft: "15px",
-  marginRight: "15px",
-  paddingLeft: '10px',
-  paddingRight: '10px'
+  padding: '0 30px 0 30px',
+  margin: '20px 0 0 0',
 };
 
 const chatLogo = {
@@ -216,47 +182,12 @@ const helloStyle = {
   flexDirection: 'column',
   alignItems: 'center',
   justifyContent: 'center',
-  marginTop: '30vmin'
+  marginTop: '20px'
 };
 
 const logoStyle = {
   height: '40vmin',
   margin: '-70px -70px -50px -70px'
-};
-
-const messageStyle = {
-  backgroundColor: '#D9D9D9',
-  borderRadius: '25px',
-  display: 'flex',
-  justifyContent: 'center',
-  position: 'absolute',
-  right: '20px',
-  top: '550px',
-  padding: '10px',
-};
-
-const MessageIAStyle = {
-  backgroundColor: '#878BCB',
-  borderRadius: '25px',
-  display: 'flex',
-  justifyContent: 'center',
-  position: 'absolute',
-  left: '305px',
-  top: '600px',
-  padding: '10px',
-  // max width
-  width: 'calc(10px + 100vmin)',
-};
-
-const messsageBoxStyle = {
-  backgroundColor: '#D9D9D9',
-  borderRadius: '50px',
-  width: 'calc(10px + 100vmin)',
-  display: 'flex',
-  justifyContent: 'center',
-  color: '#8C8282',
-  position: 'absolute',
-  bottom: '20px',
 };
 
 export default ChatPage;
--- src/components/Conversation.js
+++ src/components/Conversation.js
@@ -22,7 +22,7 @@ const Conversation = ({ conversationId }) => {
         if (message.trim() === '') return;
 
         const userMessage = { user_message: message, bot_response: '' };
-        setMessages([...messages, userMessage]);
+        setMessages(prevMessages => [...prevMessages, userMessage]);
 
         fetch('https://travel-mate-2.azurewebsites.net/chat', {
             method: 'POST',
@@ -36,8 +36,11 @@ const Conversation = ({ conversationId }) => {
                 return response.json();
             })
             .then(data => {
-                const updatedMessages = [...messages, { user_message: message, bot_response: data.bot_response }];
-                setMessages(updatedMessages);
+                setMessages(prevMessages => {
+                    const updatedMessages = [...prevMessages];
+                    updatedMessages[updatedMessages.length - 1].bot_response = data.bot_response;
+                    return updatedMessages;
+                });
                 setMessage('');
             })
             .catch(error => setError(error.toString()));
@@ -45,27 +48,78 @@ const Conversation = ({ conversationId }) => {
 
     return (
         <div>
-            <h1>Conversation {conversationId}</h1>
             {error && <div style={{ color: 'red' }}>{error}</div>}
-            <div id="chat-container" style={{ maxWidth: '600px', margin: '0 auto', padding: '10px', border: '1px solid #ccc', borderRadius: '5px', overflowY: 'auto', height: '400px' }}>
+            <div id="chat-container" style={{ maxWidth: '1100px', padding: '10px', overflowY: 'auto', height: '300px' }}>
                 {messages.map((msg, index) => (
-                    <div key={index} className={`message ${msg.user_message ? 'user-message' : 'bot-message'}`} style={{ margin: '10px 0', textAlign: msg.user_message ? 'right' : 'left', color: msg.user_message ? 'blue' : 'green' }}>
-                        {msg.user_message || msg.bot_response}
+                    <div key={index} className={`message ${msg.user_message ? 'user-message' : 'bot-message'}`} >
+                        {msg.user_message && <div style={messageStyle}>{msg.user_message}</div>}
+                        {msg.bot_response && <div style={messageIAStyle}>{msg.bot_response}</div>}
                     </div>
                 ))}
             </div>
-            <form id="chat-form" onSubmit={sendMessage}>
+            <form id="chat-form" onSubmit={sendMessage} style={messsageBoxStyle}>
                 <input
                     type="text"
                     id="message"
                     value={message}
                     onChange={(e) => setMessage(e.target.value)}
-                    placeholder="Entrez votre message"
+                    placeholder="Message à TravelIA..."
+                    style={inputStyle}
                 />
-                <button type="submit">Envoyer</button>
+                <button type="submit" style={buttonStyle}>Envoyer</button>
             </form>
         </div>
     );
 };
+
+const messageStyle = {
+    backgroundColor: '#D9D9D9',
+    padding: '10px',
+    borderRadius: '25px',
+    margin: '10px 0',
+    color: 'black',
+    textAlign: 'right',
+    maxWidth: '80%',
+    marginLeft: 'auto',
+  };
+  
+const messageIAStyle = {
+    backgroundColor: '#878BCB',
+    padding: '10px',
+    borderRadius: '25px',
+    margin: '10px 0',
+    color: 'black',
+    textAlign: 'left',
+    maxWidth: '80%',
+};  
+
+const messsageBoxStyle = {
+    backgroundColor: '#D9D9D9',
+    borderRadius: '50px',
+    width: '70%',
+    display: 'flex',
+    justifyContent: 'center',
+    color: '#8C8282',
+    position: 'absolute',
+    bottom: '20px',
+    right: '70px',
+  };
+
+  const inputStyle = {
+    border: 'none',
+    borderRadius: '25px',
+    padding: '10px',
+    flexGrow: 1,
+    fontSize: '16px',
+    backgroundColor: '#D9D9D9',
+  };
+
+    const buttonStyle = {
+        borderRadius: '25px',
+        padding: '10px 15px',
+        border: 'none',
+        color: 'white',
+        cursor: 'pointer',
+    };
 
 export default Conversation;
